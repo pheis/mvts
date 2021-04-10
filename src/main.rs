7e6a2f7dd356497b02d1cefa3e70c43c8f6c129a
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2021-04-10 18:46:06 +0300

    Hack surrounding chars away

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::array;
 use std::env;
 use std::fs;
 use std::vec;
@@ -12,7 +13,6 @@ extern "C" {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    // println!("{:?}", args);
     match args.len() {
         0 | 1 => println!("Gimme file"),
         _ => read_file(&args[1]),
@@ -36,42 +36,77 @@ fn read_file(file_name: &String) -> () {
     let source_code = fs::read_to_string(file_name).expect("Unable to read file");
     let language = infer_langauge_from_suffix(&file_name);
 
-    let imports = get_top_level_imports(source_code.clone(), language);
+    query_imports(source_code.clone(), language);
+
+    let imports = query_imports(source_code.clone(), language);
 
     let text = Rope::from_str(&source_code);
 
-    for (sp, ep) in imports {
-        let start_idx = text.line_to_char(sp.row) + sp.column;
-        let end_idx = text.line_to_char(ep.row) + ep.column;
+    for (start_point, end_point) in imports {
+        let start_idx = text.line_to_char(start_point.row) + start_point.column + 1;
+        let end_idx = text.line_to_char(end_point.row) + end_point.column - 1;
 
-        println!("{}", text.slice(start_idx..end_idx));
+        let source = text.slice(start_idx..end_idx);
+
+        let source_string = source.to_string();
+
+        if !source_string.starts_with(".") {
+            continue;
+        }
+        println!("{}", source);
     }
 }
 
-fn get_top_level_imports(source_code: String, language: Language) -> Vec<(Point, Point)> {
+fn get_tree(source_code: String, language: Language) -> Tree {
     let mut parser = Parser::new();
     parser.set_language(language).unwrap();
+    parser.parse(source_code, None).unwrap()
+}
 
-    let tree = parser.parse(source_code, None).unwrap();
-
+fn query_imports(source_code: String, language: Language) -> Vec<(Point, Point)> {
+    let tree = get_tree(source_code, language);
     let root_node = tree.root_node();
-    let mut cursor = tree.walk();
+
+    let query = Query::new(language, "(import_statement (string) @import)").expect("bad query");
+
+    let mut query_cursor = QueryCursor::new();
 
     let mut imports = vec![];
 
-    for node in root_node
-        .children(&mut cursor)
-        .filter(|node| node.kind() == "import_statement")
-    {
-        let mut child_cursor = node.walk();
-        for child in node
-            .children(&mut child_cursor)
-            .filter(|child| child.kind() == "string" && !child.is_extra())
-        {
-            let sp = child.start_position();
-            let ep = child.end_position();
-            imports.push((sp.clone(), ep.clone()))
+    // TODO what does callback do?
+    for (query_matches, u) in query_cursor.captures(&query, root_node, |_| "What does this do?") {
+        let captures = query_matches.captures;
+        for i in 0..(u + 1) {
+            let node = captures[i].node;
+            let start_point = node.start_position();
+            let end_point = node.end_position();
+            imports.push((start_point.clone(), end_point.clone()))
         }
     }
     imports
 }
+
+// fn get_top_level_imports(source_code: String, language: Language) -> Vec<(Point, Point)> {
+//     let tree = get_tree(source_code, language);
+
+//     let root_node = tree.root_node();
+//     let mut cursor = tree.walk();
+
+//     let mut imports = vec![];
+
+//     for node in root_node
+//         .children(&mut cursor)
+//         .filter(|node| node.kind() == "import_statement")
+//     {
+//         let mut child_cursor = node.walk();
+//         for child in node
+//             .children(&mut child_cursor)
+//             .filter(|child| child.kind() == "string" && !child.is_extra())
+//         {
+//             let start_point = child.start_position();
+//             let end_point = child.end_position();
+//             imports.push((start_point.clone(), end_point.clone()))
+//         }
+//     }
+//     imports
+// }
