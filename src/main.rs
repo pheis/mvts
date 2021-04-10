e8adc69bfb149c08cb163395971232ccf2514eff
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2021-04-10 14:21:29 +0300

    Print imports

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 use std::env;
 use std::fs;
+use std::vec;
 
-use tree_sitter::{Language, Parser, Query, QueryCursor, Tree, TreeCursor};
+use ropey::Rope;
+use tree_sitter::{Language, Parser, Point, Query, QueryCursor, Tree, TreeCursor};
+
+// ropey
+// text.line_to_char || line_to_byte
+
+// -> start_idx = line_to_char(sp[0]) + sp[1]
+// -> end_id = line_to_char(ep[0] + ...)
+//
+//
 
 extern "C" {
     fn tree_sitter_typescript() -> Language;
@@ -33,61 +43,34 @@ fn read_file(file_name: &String) -> () {
     let source_code = fs::read_to_string(file_name).expect("Unable to read file");
     let language = infer_langauge_from_suffix(&file_name);
 
-    parse_program(source_code, language);
+    let imports = get_top_level_imports(source_code.clone(), language);
+
+    let text = Rope::from_str(&source_code);
+
+    for (sp, ep) in imports {
+        let start_idx = text.line_to_char(sp.row) + sp.column;
+        let end_idx = text.line_to_char(ep.row) + ep.column;
+
+        println!("{}", text.slice(start_idx..end_idx));
+    }
+    //
+    // imports.into_iter().collect
+
+    // for line in source_code.lines() {
+    //     println!("{} ", line);
+    // }
 }
 
-fn parse_program(source_code: String, language: Language) -> () {
+fn get_top_level_imports(source_code: String, language: Language) -> Vec<(Point, Point)> {
     let mut parser = Parser::new();
     parser.set_language(language).unwrap();
 
     let tree = parser.parse(source_code, None).unwrap();
-    // get_top_level_imports(tree);
-    list_imports(tree);
-    // query_magic(tree, language);
 
-    // let root_node = tree.root_node();
-
-    // let mut cursor = tree.walk();
-    // cursor.goto_first_child();
-    // println!("{}", cursor.node().kind());
-
-    // println!("{}", root_node.kind());
-
-    // let q = Query::new(language, "(import_statement :source)").expect("lol");
-    // let mut qc = QueryCursor::new();
-    // let lol = qc.matches(&q, root_node, |node| node.kind());
-    // for item in lol {
-    //     for x in item.captures {
-    //         println!("{}", x.node.kind());
-    //     }
-    // }
-}
-
-fn get_top_level_imports(tree: Tree) {
-    let mut cursor = tree.walk();
-    cursor.goto_first_child();
-    loop {
-        let node = cursor.node();
-        if node.kind() == "import_statement" {
-            for source_node in node
-                .children(&mut node.walk())
-                .filter(|child_node| child_node.kind() == "string")
-            {
-                println!("{}", source_node.child_count());
-                println!("{}", source_node.is_named());
-            }
-        }
-
-        let has_sibling = cursor.goto_next_sibling();
-        if !has_sibling {
-            break;
-        }
-    }
-}
-
-fn list_imports(tree: Tree) {
     let root_node = tree.root_node();
     let mut cursor = tree.walk();
+
+    let mut imports = vec![];
 
     for node in root_node
         .children(&mut cursor)
@@ -100,9 +83,8 @@ fn list_imports(tree: Tree) {
         {
             let sp = child.start_position();
             let ep = child.end_position();
-            println!("{}", child.kind());
-            println!("{}", sp);
-            println!("{}", ep);
+            imports.push((sp.clone(), ep.clone()))
         }
     }
+    imports
 }
