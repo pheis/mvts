0a06c5f3e0487a2154e75a36a8cbff3bbbaeacb4
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2021-05-08 11:52:25 +0300

    Use ignore instead of walkdir

--- src/grep.rs
+++ src/grep.rs
@@ -1,58 +1,32 @@
-use anyhow::{anyhow, Result};
+use anyhow::Result;
+use ignore::Walk;
 use std::fs;
 use std::path::PathBuf;
-use walkdir::{DirEntry, WalkDir};
 
 use crate::import_string;
 use crate::path;
 
-fn is_ok_file(entry: &DirEntry) -> bool {
-    entry
-        .file_name()
-        .to_str()
-        .map(|s| {
-            let is_hidden_file = !s.eq(".") && s.starts_with('.');
-            let is_node_module = s.eq("node_modules");
-            !is_hidden_file & !is_node_module
-        })
-        .unwrap_or(false)
-}
-
 pub fn find_affected_files(current_dir: &PathBuf, moved_file: &PathBuf) -> Result<Vec<PathBuf>> {
     let full_moved_path = path::join(current_dir, moved_file)?;
 
-    let walker = WalkDir::new(".")
+    let affected_files = Walk::new(current_dir)
         .into_iter()
-        .filter_entry(|e| is_ok_file(e))
-        .filter_map(|e| e.ok())
-        .filter(|e| {
-            e.file_name()
+        .filter_map(|result| result.ok())
+        .filter(|entry| {
+            entry
+                .file_name()
                 .to_str()
-                .map(|s| s.ends_with(".ts") || s.ends_with(".tsx"))
+                .map(|file_name| file_name.ends_with(".ts") || file_name.ends_with(".tsx"))
                 .unwrap_or(false)
         })
-        .filter(|entry| {
-            let full_path = current_dir.join(entry.path());
-            path::normalize(&full_path)
-                .map_err(|_| anyhow!("Failed to normalize path {:?}", entry.path()))
-                .and_then(|file_path| has_import_to_file(&file_path, &full_moved_path))
-                .unwrap_or(false)
-        });
+        .map(|entry| entry.path().to_path_buf())
+        .filter(|affected_file| !affected_file.eq(&full_moved_path))
+        .filter(|affected_file| {
+            has_import_to_file(&affected_file, &full_moved_path).unwrap_or(false)
+        })
+        .collect();
 
-    let mut files = vec![];
-
-    for entry in walker {
-        let full_entry_path = current_dir.join(entry.path());
-        let full_entry_path = path::normalize(&full_entry_path)?;
-
-        if full_entry_path.eq(&full_moved_path) {
-            continue;
-        }
-
-        files.push(entry.path().to_path_buf());
-    }
-
-    Ok(files)
+    Ok(affected_files)
 }
 
 fn has_import_to_file(source_file: &PathBuf, imported_file: &PathBuf) -> Result<bool> {
